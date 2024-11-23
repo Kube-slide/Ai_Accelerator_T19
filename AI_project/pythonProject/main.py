ff376d1c023a655d5c91958e8d44ca7749237f24
KubeSlide <rajaabdul006@gmail.com>
2024-11-23 11:46:29 -0500

    Formatted data
    - Formatted all current .csv files to be read by pandas
    - Imported all .csv files into main.py as variables

--- AI_project/pythonProject/main.py
+++ AI_project/pythonProject/main.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
-
 import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+income = pd.read_csv('Data/Income_Industry.csv', delimiter=',')
+jobPrediction = pd.read_csv('Data/Occupation_Projections.csv', delimiter=',')
+medianIncome = pd.read_csv('Data/Median_Income.csv', delimiter=',')
+tuitionCost = pd.read_csv('Data/Tuition_Cost.csv', delimiter=',')
+enrollment = pd.read_csv('Data/Enrollment.csv', delimiter=',')
+graduates = pd.read_csv('Data/Graduates.csv', delimiter=',')
